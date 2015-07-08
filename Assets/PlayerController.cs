1fbe128c4d83bb39186a27cefad71038d24cc02d
Sharlene <17sharl@gmail.com>
2015-07-08 21:32:09 +0800

    Add timer countdown

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,37 +3,59 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	
 	public Text countText;
-	public Text winText;
+	public Text timerText;
+	public Text playButtonText;
+	public float timeRemaining;
 	private int count;
-
-	public void Start ()
-	{
+	private bool gameRunning=true;
+	
+	void Start() {
+		InvokeRepeating ("decreaseTimeRemaining", 1.0F, 1.0F);
 		countText.color = Color.blue;
 		countText.text = "Old Score: " + RetrieveScore ().ToString ();
 		count = 0;
 	}
-
+	
+	void Update() {
+		if (timeRemaining == 0)
+		{
+			timeElapsed();
+		}
+		
+		timerText.text = timeRemaining + " seconds remaining!";
+	}
+	
+	// Countdown
+	void decreaseTimeRemaining() {
+		timeRemaining --;
+	}
+	
+	// Gameover
+	void timeElapsed(){
+		CancelInvoke ("decreaseTimeRemaining");
+		playButtonText.text = "Game Over";
+		gameRunning = false;
+	}
+	
+	// Set score
 	public void SetScoreText ()
 	{
-		count += 1;
-		SaveScore (count);
-
-		countText.color = Color.black;
-		countText.text = "Score: " + count.ToString ();
-		if (count >= 12)
-		{
-			winText.text = "You Win!";
+		if (gameRunning == true) {
+			count += 1;
+			countText.text = "Score: " + count.ToString ();
+			countText.color = Color.black;
+			SaveScore(count);
 		}
 	}
-
+	
 	private void SaveScore (int score) 
 	{
 		PlayerPrefs.SetInt ("Score", score);
 		PlayerPrefs.Save ();
 	}
-
+	
 	/*
 	 * Returns the previous score. If previous score does not exist, it returns 0.
 	 */
@@ -43,10 +65,12 @@ public class PlayerController : MonoBehaviour {
 		{
 			return PlayerPrefs.GetInt ("Score");
 		}
-
+		
 		else
 		{
 			return 0;
 		}
 	}
 }
+
+
