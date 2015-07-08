6e8f00e4f3fbe970644b329417ff11116b748e3e
Kevin Lee <kvnlte@gmail.com>
2015-07-08 16:36:02 +0800

    Implemented saving of score

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,16 +6,45 @@ public class PlayerController : MonoBehaviour {
 
 	public Text countText;
 	public Text winText;
-	private int count=0;
+	private int count;
 
-	
+	public void Start ()
+	{
+		countText.text = "Old Score: " + RetrieveScore ().ToString ();
+		count = 0;
+	}
+
 	public void SetScoreText ()
 	{
 		count += 1;
+		SaveScore (count);
+
 		countText.text = "Score: " + count.ToString ();
 		if (count >= 12)
 		{
 			winText.text = "You Win!";
+		}
+	}
+
+	private void SaveScore (int score) 
+	{
+		PlayerPrefs.SetInt ("Score", score);
+		PlayerPrefs.Save ();
+	}
+
+	/*
+	 * Returns the previous score. If previous score does not exist, it returns 0.
+	 */
+	private int RetrieveScore ()
+	{
+		if (PlayerPrefs.HasKey ("Score")) 
+		{
+			return PlayerPrefs.GetInt ("Score");
+		}
+
+		else
+		{
+			return 0;
 		}
 	}
 }
