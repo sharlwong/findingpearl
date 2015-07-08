3bbfbfb83234d5536ef0f86ca92f86c151b51c5e
Sharlene <17sharl@gmail.com>
2015-07-08 15:58:58 +0800

    Increment score when button is clicked

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Text countText;
+	public Text winText;
+	private int count=0;
+
+	
+	public void SetScoreText ()
+	{
+		count += 1;
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
