05277f674e16e805097b0cfb2d54ee66647a074b
unknown <Immanuella_lim@mymail.sutd.edu.sg>
2015-07-29 23:41:38 +0800

    added transition between scenes, all scenes now have to use StoryChangeScene instead of change scene, or else it cannot transition to the next next screen sigh, will integrate with level 1 and level 2

---
+++ Assets/Scripts/StoryChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryChangeScene : MonoBehaviour {
+
+	public void ChangeToScene(string sceneToChangeTo) {
+		float fadeTime = GameObject.Find("_Manager").GetComponent<Fading>().BeginFade(1);
+		Application.LoadLevel(sceneToChangeTo);
+	}
+}
