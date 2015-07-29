df8faba7fbff4d0015545d5b49064f0c967e4a30
unknown <Immanuella_lim@mymail.sutd.edu.sg>
2015-07-29 22:42:08 +0800

    added storyboard scenes, music now plays throughout from scene1 to scene5 will do screen fading next

---
+++ Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusic : MonoBehaviour {
+
+	static bool AudioBegin = false; 
+	
+	void Awake()
+	{
+		if (!AudioBegin) {
+			GetComponent<AudioSource>().Play ();
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		} 
+	}
+	
+	void Update () {
+		if(Application.loadedLevelName == "RopeScene")
+		{
+			GetComponent<AudioSource>().Stop();
+			AudioBegin = false;
+		}
+		
+	}
+}
