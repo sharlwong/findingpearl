511ba2b21d986cd0f9cd28aff51cd0ba7fad82dc
Sharlene <17sharl@gmail.com>
2015-07-29 20:30:10 +0800

    Fix pink screen problem

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScenes : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeToScene(string sceneToChangeTo){
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
