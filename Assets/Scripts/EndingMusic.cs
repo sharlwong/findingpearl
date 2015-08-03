2499945bd49ad06393803e00b2cd58f8906dbf8b
unknown <Immanuella_lim@mymail.sutd.edu.sg>
2015-08-03 19:35:37 +0800

    added ending scenes

---
+++ Assets/Scripts/EndingMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingMusic : MonoBehaviour {
+
+	static bool AudioBegin = false; 
+	
+	void Awake()
+	{
+		if (!AudioBegin) {
+			GetComponent<AudioSource>().Play ();
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		} 
+	}
+	
+	void Update () {
+		if(Application.loadedLevelName == "StartScene")
+		{
+			GetComponent<AudioSource>().Stop();
+			AudioBegin = false;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/GetLevelScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetLevelScore : MonoBehaviour {
+
+	private string levelText;
+	private Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		levelText = GetComponentsInChildren<Text>()[0].text;
+		scoreText = GetComponentsInChildren<Text>()[1];
+
+		scoreText.text = "RP: " + PlayerPrefs.GetInt(levelText).ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
