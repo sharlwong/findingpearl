027b1f5d649dd43a97dcb7bbf191fc84292d8f7a
unknown <Immanuella_lim@mymail.sutd.edu.sg>
2015-08-06 02:08:54 +0800

    StartScene start button and story button now calls specialized functions newly put into the StoryChangeScene, which has been modified. -if player is playing for the first time, pressing the start button launches the story, else it launches level selection screen -if player hits the story button, the story launches, but at StoryScene5, it loops back to the StartScene.

--- Assets/Scripts/StoryChangeScene.cs
+++ Assets/Scripts/StoryChangeScene.cs
@@ -3,7 +3,41 @@ using System.Collections;
 
 public class StoryChangeScene : MonoBehaviour {
 
+	void Start() {
+		// PlayerPrefs.DeleteAll();
+	}
+
 	public void ChangeToScene(string sceneToChangeTo) {
+
+		if (Application.loadedLevelName == "StoryScene5") {
+			if (PlayerPrefs.GetInt("StoryActive") == 1) {
+				PlayerPrefs.SetInt("StoryActive", 0);
+				PlayerPrefs.Save();
+				TransitAndLoad("StartScene");
+				return; // important to return from method!
+			} 
+		}
+	
+		TransitAndLoad(sceneToChangeTo);
+	}
+
+	public void OnStartButtonEnter() {
+		if (PlayerPrefs.HasKey("FirstTimePlayer")) {
+			ChangeToScene("LevelSelectionScene");
+		} else {
+			PlayerPrefs.SetInt("FirstTimePlayer", 1);
+			PlayerPrefs.Save();
+			ChangeToScene("StoryScene1");
+		}
+	}
+
+	public void OnStoryButtonEnter() {
+		PlayerPrefs.SetInt("StoryActive", 1);
+		PlayerPrefs.Save();
+		ChangeToScene("StoryScene1");
+	}
+
+	private void TransitAndLoad(string sceneToChangeTo) {
 		GameObject.Find("_Manager").GetComponent<Fading>().BeginFade(1);
 		Application.LoadLevel(sceneToChangeTo);
 	}
