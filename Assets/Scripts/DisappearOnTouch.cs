fc798cc45a424481ec02b0759f942f0dd18e333e
Sharlene <17sharl@gmail.com>
2015-08-06 01:04:23 +0800

    Make the Pull image for tutorial disappear on touch

---
+++ Assets/Scripts/DisappearOnTouch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour {
+
+	// Pull GameObject will disappear upon the first touch of the user
+	void Update() {
+		if (Input.anyKeyDown) {
+			Destroy (this.gameObject);
+		}	
+	}
+}
